5047da94f75d044b9471301862aacd44836b7fb1
meganemura <meganemura@users.noreply.github.com>
2015-09-07 01:48:34 +0900

    Move view implementation from model to component-view

--- src/index.js
+++ src/index.js
@@ -12,121 +12,134 @@ var initialRepositories = [
 ]
 var initialRepository = initialRepositories[Math.floor(initialRepositories.length * Math.random())]
 
-var GitHubEvent = function(data) {
-  this.data = data;
+var GitHubEvent = {}
+GitHubEvent.repositoryEvents = function(owner_and_repo) {
+  return m.request({dataType: "jsonp", url: ghApiEndpoint + "repos/" + owner_and_repo + "/events"});
+};
 
-  this.event = function() {
-    switch (this.data.type) {
-      case 'ForkEvent':         return [this.login(), "forked"]
-      case 'GollumEvent':       return this.gollumEvent()
-      case 'IssueCommentEvent': return this.issueCommentEvent()
-      case 'IssuesEvent':       return this.issuesEvent()
-      case 'PullRequestEvent':  return this.pullRequestEvent()
-      case 'PullRequestReviewCommentEvent': return this.pullRequestReviewCommentEvent()
-      case 'PushEvent':         return this.pushEvent()
-      case 'WatchEvent':        return [this.login(), "starred"]
-      default: return [
-        this.login(),
-        m('span', {class: 'text-muted'}, [
-          "('",
-          this.data.type,
-          "' Not Implemented)",
-        ]),
-      ];
-    }
-  };
+// Username with gravatar
+var UserName = {
+  view: function(ctrl, args) {
+    return m('a', {href: ghUrl + args.data.actor.login}, [
+      m('img', {src: args.data.actor.avatar_url, width: 20, class: 'img-rounded', hspace: 6}),
+      args.data.actor.login,
+      ' ',
+    ]);
+  },
+}
 
-  this.gollumEvent = function() {
-    return [
-      this.login(),
-      this.data.payload.pages.map(function(page) {
+var ForkEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
+      "forked",
+    ])
+  },
+}
+
+var GollumEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
+      args.data.payload.pages.map(function(page) {
         return [
           page.action,
           ' ',
           m('a', {href: ghUrl + page.html_url}, page.title),
         ];
       }),
-    ];
-  };
+    ])
+  },
+}
 
-  this.issueCommentEvent = function() {
-    return [
-      this.login(),
+var IssueCommentEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
       ' commented on issue ',
-      m('a', {href: this.data.payload.comment.html_url}, [
+      m('a', {href: args.data.payload.comment.html_url}, [
         '#',
-        this.data.payload.issue.number,
+        args.data.payload.issue.number,
       ]),
-    ];
-  };
+    ])
+  },
+}
 
-  this.issuesEvent = function() {
-    return [
-      this.login(),
+var IssuesEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
       ' ',
-      this.data.payload.action,
+      args.data.payload.action,
       ' issue ',
-      m('a', {href: this.data.payload.issue.html_url}, [
+      m('a', {href: args.data.payload.issue.html_url}, [
         '#',
-        this.data.payload.issue.number,
+        args.data.payload.issue.number,
       ]),
-    ];
-  };
+    ])
+  },
+}
 
-  this.pullRequestEvent = function() {
-    return [
-      this.login(),
+var PullRequestEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
       ' ',
-      this.data.payload.action,
+      args.data.payload.action,
       ' pull request ',
-      m('a', {href: this.data.payload.pull_request.html_url}, [
+      m('a', {href: args.data.payload.pull_request.html_url}, [
         '#',
-        this.data.payload.pull_request.number,
+        args.data.payload.pull_request.number,
       ]),
-    ];
-  };
+    ])
+  },
+}
 
-  this.pullRequestReviewCommentEvent = function() {
-    return [
-      this.login(),
+var PullRequestReviewCommentEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
       ' commented on pull request ',
-      m('a', {href: this.data.payload.comment.html_url}, [
+      m('a', {href: args.data.payload.comment.html_url}, [
         '#',
-        this.data.payload.pull_request.number,
+        args.data.payload.pull_request.number,
       ]),
-    ];
-  };
+    ])
+  },
+}
 
-  this.pushEvent = function() {
-    var numOfCommits = this.data.payload.commits.length;
-    return [
-      this.login(),
+var PushEvent = {
+  view: function(ctrl, args) {
+    var numOfCommits = args.data.payload.commits.length;
+    return m('span', [
+      m.component(UserName, {data: args.data}),
       ' pushed ',
       numOfCommits.toString(),
       ' commit' + ((numOfCommits == 1) ? '' : 's'),
-    ];
-  };
+    ])
+  },
+}
 
-  // icon and name with link
-  this.login = function() {
-    return m('a', {href: ghUrl + this.data.actor.login}, [
-      m('img', {src: data.actor.avatar_url, width: 20, class: 'img-rounded', hspace: 6}),
-      data.actor.login,
-      ' ',
-    ]);
-  };
+var WatchEvent = {
+  view: function(ctrl, args) {
+    return m('span', [
+      m.component(UserName, {data: args.data}),
+      "starred",
+    ])
+  },
+}
 
-  this.icon = function() {
+var EventIcon = {
+  view: function(ctrl, args) {
     return m('span', {
-      class: 'octicon ' + this.octiconClass(),
-      title: this.data.type,
+      class: 'octicon ' + this.octiconClass(args.data.type),
+      title: args.data.type,
       'text-align': 'center',
     });
-  };
+  },
 
-  // returns octicon style class
-  this.octiconClass = function() {
-    switch (this.data.type) {
+  octiconClass: function(type) {
+    switch (type) {
       case 'CreateEvent':       return 'octicon-git-pull-request';
       case 'DeleteEvent':       return 'octicon-trashcan';
       case 'ForkEvent':         return 'octicon-repo-forked';
@@ -139,11 +152,8 @@ var GitHubEvent = function(data) {
       case 'WatchEvent':        return 'octicon-star';
       default: return 'octicon-question';
     }
-  };
-};
-GitHubEvent.repositoryEvents = function(owner_and_repo) {
-  return m.request({dataType: "jsonp", url: ghApiEndpoint + "repos/" + owner_and_repo + "/events"});
-};
+  },
+}
 
 var vm = {
   init: function() {
@@ -164,6 +174,28 @@ var vm = {
       });
     };
     vm.events = m.prop([]);
+
+    vm.dispatchEvent = function(event) {
+      switch (event.type) {
+        case 'ForkEvent':                     return m.component(ForkEvent,                      {data: event})
+        case 'GollumEvent':                   return m.component(GollumEvent,                    {data: event})
+        case 'IssueCommentEvent':             return m.component(IssueCommentEvent,              {data: event})
+        case 'IssuesEvent':                   return m.component(IssuesEvent,                    {data: event})
+        case 'PullRequestEvent':              return m.component(PullRequestEvent,               {data: event})
+        case 'PullRequestReviewCommentEvent': return m.component(PullRequestReviewCommentEvent,  {data: event})
+        case 'PushEvent':                     return m.component(PushEvent,                      {data: event})
+        case 'WatchEvent':                    return m.component(WatchEvent,                     {data: event})
+        default: return [
+          m.component(UserName, {data: event}),
+          m('span', {class: 'text-muted'}, [
+            "('",
+            event.type,
+            "' Not Implemented)",
+          ]),
+        ];
+      }
+    };
+
     vm.meta = m.prop({});
     vm.rateLimit = function() {
       if ($.isEmptyObject(this.meta())) { return }
@@ -217,17 +249,16 @@ var EventListComponent = {
       ]),
       m('table', {class: 'table table-condensed'}, [
         m('tbody', [
-          vm.events().map(function(data) {
-            var e = new GitHubEvent(data);
-            var created_at = moment(data.created_at)
+          vm.events().map(function(event) {
+            var created_at = moment(event.created_at)
             return [
               m('tr', [
-                m('td', {align: 'center', class: 'onepx'}, e.icon()),
+                m('td', {align: 'center', class: 'onepx'}, m.component(EventIcon, {data: event})),
                 m('td', [
-                  e.event(),
+                  vm.dispatchEvent(event),
                   ' ',
                   m('span', {class: 'text-muted', title: created_at.toString()},
-                    moment(data.created_at).fromNow()
+                    moment(event.created_at).fromNow()
                   ),
                 ]),
               ]),
