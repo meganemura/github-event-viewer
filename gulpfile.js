6d377e23bf8e596bc1eda8f55c82da8ae230c610
meganemura <meganemura@users.noreply.github.com>
2015-09-08 01:04:26 +0900

    Use scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@ var gulp = require('gulp');
 var ghpages = require('gh-pages');
 var path = require('path');
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['sass'], function() {
   return ghpages.publish(path.join(__dirname, 'src'), function(err) {
     console.log(err);
   });
+});
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./src/stylesheets/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/stylesheets'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/stylesheets/**/*.scss', ['sass']);
 });
